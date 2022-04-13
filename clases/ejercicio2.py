e558e044b992b29c7c75afe92decc85504a22e98
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-13 13:45:40 +0200

    .

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -1,7 +1,17 @@
-from introducir import solicitar_introducir_cadena
 
-__texto = solicitar_introducir_cadena("Introduce un texto") #entrada éstandar. Digamos que una línea vale 100 caracteres.
-__caracteres = __texto.split("")
-print(__caracteres)
 archivo = open("Mayúsculas.txt", 'w', encoding='utf-8')
+archivo.close()
+__texto = input("Introduce un texto") #entrada éstandar. Digamos que una línea vale 100 caracteres.
+__caracteres = list(__texto)
+archivo = open("Mayúsculas.txt", 'a', encoding='utf-8')
+n=1
+while n<3:
+    if len(__caracteres) < 101:
+        archivo.write(__texto)
+        break
+    else:
+        for i in range(101):
+            letra = __caracteres[i].pop()
+            archivo.write(letra)  
+    n+=1    
 archivo.close()
