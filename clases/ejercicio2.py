d5639be5de6ff70718529ef52c0d42e9c1e215b7
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-13 12:57:33 +0200

    Update ejercicio2.py

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -1,3 +1,5 @@
-archivo = open("Mayúsculas.txt", 'w', encoding='utf-8')
+from introducir import solicitar_introducir_cadena
 
+archivo = open("Mayúsculas.txt", 'w', encoding='utf-8')
+texto = solicitar_introducir_cadena("Introduce un texto")
 archivo.close()
