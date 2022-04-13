6df0860695fe91f64f68da47c4cdc9d4ad3693e7
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-13 13:25:50 +0200

    .

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -1,5 +1,7 @@
 from introducir import solicitar_introducir_cadena
 
 __texto = solicitar_introducir_cadena("Introduce un texto") #entrada éstandar. Digamos que una línea vale 100 caracteres.
+__caracteres = __texto.split("")
+print(__caracteres)
 archivo = open("Mayúsculas.txt", 'w', encoding='utf-8')
 archivo.close()
