c89d0cd85b882ad100a86eaed062b4105fbd8b79
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-14 18:09:30 +0200

    .

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -1,23 +1,35 @@
 from introducir.cadena import solicitar_introducir_cadena
 
-def doslineas():
-    archivo = open("Mayúsculas.txt", 'w', encoding='utf-8')
-    archivo.write("")
-    archivo.close()
-    __texto = solicitar_introducir_cadena("Introduce un texto") #entrada éstandar. Digamos que una línea vale 100 caracteres.
-    __caracteres = list(__texto)
-    archivo = open("Mayúsculas.txt", 'a', encoding='utf-8')
-    for j in range(2):
-        if len(__caracteres) < 101:
-            archivo.write(str(__texto))
-            break
-        else:
-            for i in range(100):
-                letra = __caracteres[i]
-                if i == 99:
-                    archivo.write(str(letra)+"\n")
-                else:
-                    archivo.write(letra)
-            for k in range(100):
-                __caracteres.pop(0) 
-    archivo.close()
+class Modelo:
+    def __init__(self, texto):
+        self.texto = texto
+class Vista:
+    def __init__(self):
+        self.modelo = Modelo(solicitar_introducir_cadena("Introduce un texto") #entrada éstandar. Digamos que una línea vale 100 caracteres.
+)
+    def doslineas(self):
+        archivo = open("Mayúsculas.txt", 'w', encoding='utf-8')
+        archivo.write("")
+        archivo.close()
+        __caracteres = list(self.modelo)
+        archivo = open("Mayúsculas.txt", 'a', encoding='utf-8')
+        for j in range(2):
+            if len(__caracteres) < 101:
+                archivo.write(str(self.modelo).upper())
+                break
+            else:
+                for i in range(100):
+                    letra = __caracteres[i]
+                    if i == 99:
+                        archivo.write(str(letra).upper()+"\n")
+                    else:
+                        archivo.write(letra.upper())
+                for k in range(100):
+                    __caracteres.pop(0) 
+        archivo.close()
+class Controlador:
+    def __init__(self):
+        self.vista = Vista()
+    
+    def conseguirtexto(self):
+        self.vista.doslineas()
